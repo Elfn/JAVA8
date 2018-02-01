2cdd0aafdc0862c22e5e1cb14b8a67df19ba8f27
Elimane Fofana <Elimane.Fofana@supinfo.com>
2018-02-02 00:39:20 +0100

    regex and string object

--- FUNCTIONAL_PROGRAMMING/src/Main_.java
+++ FUNCTIONAL_PROGRAMMING/src/Main_.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
-public class Main {
+public class Main_ {
 
     public static void main(String[] args) throws IOException {
 
---
+++ STRING_OBJECT_AND_REGEX/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Elimane on Feb, 2018, at 00:35
+ */
+public class Main {
+    public static void main(String [] args)
+    {
+
+    }
+}

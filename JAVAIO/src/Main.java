31bb383e30f52bbea4fd9662f01f2075a29099f9
Elimane Fofana <Elimane.Fofana@supinfo.com>
2018-07-23 03:45:53 +0200

    file attributes

---
+++ JAVAIO/src/Main.java
@@ -0,0 +1,118 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Elimane on Jul, 2018, at 02:42
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        //LISTING FILES AND DIRECTORIES
+
+        File f = new File("/Users/Elimane/Desktop/");
+
+        String[] listFiles = f.list();
+
+        System.out.println("-----------------FILESNAME-------------------");
+
+        for(String file : listFiles)
+        {
+            System.out.println(file);
+        }
+
+
+        System.out.println("-----------------DIRECTORIES LIST-------------------");
+
+        File[] listDirectories = f.listFiles();
+
+        for(File directory : listDirectories)
+        {
+            if(directory.isDirectory())
+                System.out.println(directory.getName()+" -- "+directory.getAbsolutePath());
+        }
+
+        System.out.println("-----------------FILES BY EXTENSIONS-------------------");
+
+        //FILENAMEFILTER IS AN ABSTRACT CLASS
+        FilenameFilter extensionFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+
+                if(name.endsWith("docx") || name.endsWith("pptx"))
+                    return true;
+
+                    return false;
+            }
+        };
+
+        File[] extensionsFiles = f.listFiles(extensionFilter);
+        for(File file : extensionsFiles)
+        {
+            System.out.println(file.getName()+" -- "+file.getAbsolutePath());
+        }
+
+
+        //CREATING FOLDERS AND FILES
+
+        System.out.println("-----------FOLDER CREATION");
+        if(f.exists() && f.canWrite() && f.isDirectory())
+        {
+            File newFolder = new File(f.getAbsolutePath()+"/Newfolder");
+
+            boolean isCreated = newFolder.mkdir();
+
+            if(isCreated)
+                System.out.println("Folder has been created!");
+            else
+                System.out.println("Folder creation failed!");
+
+        }
+
+        System.out.println("-----------FILE CREATION");
+        if(f.exists() && f.canWrite() && f.isDirectory())
+        {
+            File newFolder = new File(f.getAbsolutePath()+"/Newfile.txt");
+
+            boolean isCreated = false;
+            try {
+                isCreated = newFolder.createNewFile();
+                if(isCreated)
+                    System.out.println("File has been created!");
+                else
+                    System.out.println("File creation failed!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+        //ACCESSING AND MODIFIYNG FOLDER ATTRIBUTES
+
+        System.out.println("-------------ACCESSING AND MODIFIYNG FOLDER ATTRIBUTES");
+        File[] allFiles = f.listFiles();
+        for(File file : allFiles)
+        {
+            if(file.isFile())
+            {
+                long size = f.length();
+                long sizeInKB = size / 1024;
+                System.out.println(file.getName()+" -- "+sizeInKB+" KB");
+
+                long lastModified = file.lastModified();
+                Date lastModifiedTime = new Date(lastModified);
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+                System.out.println(sdf.format(lastModifiedTime));
+            }
+            //System.out.println(file.getName()+" -- "+file.getAbsolutePath());
+        }
+
+
+    }
+}
